366b254f4b723e730da9040162f3245915ed8444
Sandeep verma <itsandeepverma@gmail.com>
2023-09-24 09:21:23 +0530

    setttings fixed

--- testApi/settings.py
+++ testApi/settings.py
@@ -20,17 +20,11 @@ pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-y*0%zsihj93%db_#8ws+sb)!rw=#7%!^wt3x*_mk29z&)$_xsx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # APPEND_SLASH=False
 # Application definition
